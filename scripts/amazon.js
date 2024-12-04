a6bdcf735a99fd085ff44acb4a056de4aec33502
meatball <meatball@gmail.com>
2024-12-04 17:46:48 +0500

    generate html for amazon header and made it interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,10 @@
 import {cart, addToCart, getCartItemQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+import { renderAmazonHeader } from './amazonHeader.js';
+
+// render amazon header before page
+renderAmazonHeader();
 
 let productHTML = '';
 
